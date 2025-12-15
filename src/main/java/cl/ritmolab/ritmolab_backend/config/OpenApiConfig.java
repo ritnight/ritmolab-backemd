778d8760c87c473330da886534294be496b0c45d
Alejandra González <aleja.gonzalezc@duocuc.cl>
2025-12-15 17:56:47 -0300

    cambios en swagger

--- src/main/java/cl/ritmolab/ritmolab_backend/config/OpenApiConfig.java
+++ src/main/java/cl/ritmolab/ritmolab_backend/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package cl.ritmolab.ritmolab_backend.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,6 +14,12 @@ import org.springframework.context.annotation.Configuration;
                 version = "v1",
                 description = "API REST del backend de RitmoLab (productos, categorías, carritos, usuarios)"
         )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
 )
 public class OpenApiConfig {
 }
